ad9667e6d2a75dc6d8c50f869ac8c24559692546
clown <tt.clown@gmail.com>
2013-10-21 23:30:55 +0900

    NTP パケットの解析に必要な情報を追加

---
+++ CubeClock.Ntp/FixedPoint.cs
@@ -0,0 +1,64 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// FixedPoint.cs
+/// 
+/// Copyright (c) 2013 CubeSoft, Inc. All rights reserved.
+///
+/// MIT License
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+
+namespace CubeClock.Ntp
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Timestamp
+    ///
+    /// <summary>
+    /// 符号付き 32bit 固定小数点数から double への変換機能を提供するための
+    /// クラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public abstract class FixedPoint
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// ToDouble
+        /// 
+        /// <summary>
+        /// 符号付き 32bit 固定小数点数から double へ変換します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static double ToDouble(Int32 value)
+        {
+            var number = (Int16)(value >> 16);
+            var fraction = (UInt16)(value & Int16.MaxValue);
+            return number + fraction / _CompensatingRate16;
+        }
+
+        #region Constant variables
+        private static readonly double _CompensatingRate16 = 0x10000d;
+        #endregion
+    }
+}
---
+++ CubeClock.Ntp/Parameter.cs
@@ -0,0 +1,102 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Parameter.cs
+/// 
+/// Copyright (c) 2013 CubeSoft, Inc. All rights reserved.
+///
+/// MIT License
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+
+namespace CubeClock.Ntp
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// LeapIndicator
+    /// 
+    /// <summary>
+    /// 閏秒指示子 (LI: Leap Indicator) の状態を定義した列挙型です。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public enum LeapIndicator : uint
+    {
+        NoWarning = 0,  // 0 - No warning
+        LastMinute61,   // 1 - Last minute has 61 seconds
+        LastMinute59,   // 2 - Last minute has 59 seconds
+        Alarm           // 3 - Alarm condition (clock not synchronized)
+    }
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// VersionNumber
+    /// 
+    /// <summary>
+    /// バージョン番号 (VN: Version Number) の状態を定義した列挙型です。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public enum VersionNumber : uint
+    {
+        Unknown = 0,    // 0 - Unknown
+        Version1,       // 1 - Version 1
+        Version2,       // 2 - Version 2
+        Version3,       // 3 - Version 3 (Normally NTP server)
+        Version4        // 4 - Version 4 (Normally SNTP server)
+    }
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Mode
+    /// 
+    /// <summary>
+    /// 動作モードの状態を定義した列挙型です。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public enum Mode : uint
+    {
+        Unknown = 0,        // 0, 6, 7 - Reserved
+        SymmetricActive,    // 1 - Symmetric active
+        SymmetricPassive,   // 2 - Symmetric pasive
+        Client,             // 3 - Client
+        Server,             // 4 - Server
+        Broadcast,          // 5 - Broadcast
+    }
+
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Stratum
+    /// 
+    /// <summary>
+    /// 階層の状態を定義した列挙型です。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public enum Stratum
+    {
+        Unspecified,            // 0 - unspecified or unavailable
+        PrimaryReference,       // 1 - primary reference (e.g. radio-clock)
+        SecondaryReference,     // 2-15 - secondary reference (via NTP or SNTP)
+        Reserved                // 16-255 - reserved
+    }
+}
